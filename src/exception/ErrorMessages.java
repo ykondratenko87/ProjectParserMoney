fd3712bade43087c4548c5d5f3a5bde1af3baacb
ykondratenko87 <y.kondratenko87@gmail.com>
2024-01-18 18:31:24 +0300

    add constant in class2

--- src/exception/ErrorMessages.java
+++ src/exception/ErrorMessages.java
@@ -6,4 +6,8 @@ public class ErrorMessages {
     public static final String FILE_PARSING_ERROR_MESSAGE = "Ошибка при парсинге файлов в директории";
     public static final String FILE_PARSING_IO_ERROR_MESSAGE = "Ошибка при парсинге файла ";
     public static final String REPORT_WRITING_ERROR_MESSAGE = "Ошибка при записи отчета в файл ";
+    public static final String INVALID_ACCOUNT_NUMBER_FORMAT = "Некорректный формат номера счета в строке: ";
+    public static final String INVALID_TRANSFER_AMOUNT_FORMAT = "Некорректный формат суммы перевода в строке: ";
+    public static final String ACCOUNT_NOT_FOUND = "Такого счета нет. Пропуск операции.";
+    public static final String INSUFFICIENT_FUNDS = "Недостаточно средств на счете. Пропуск операции.";
 }
